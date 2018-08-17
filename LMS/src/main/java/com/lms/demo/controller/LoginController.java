376bc1098f13c2a0d827fa7fb23c2cca9f9cfe67
himupadh <Himanshu Upadhyay>
2018-08-17 11:34:32 +0530

    login rest endpoint

--- LMS/src/main/java/com/lms/demo/controller/LoginController.java
+++ LMS/src/main/java/com/lms/demo/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.lms.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
 public class LoginController {
+	
 
 }
